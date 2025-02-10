a7f6eb9a91766f36803efa39561790c9465159ec
weiping <weiping@atlasv.com>
2025-02-10 19:48:53 +0800

    feat: 增加一些布局公共类

--- atlasv-android-home/src/jsMain/kotlin/com/sample/Main.kt
+++ atlasv-android-home/src/jsMain/kotlin/com/sample/Main.kt
@@ -1,39 +1,72 @@
 package com.sample
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import com.sample.components.Layout
 import com.sample.core.network.HttpEngine
 import com.sample.data.model.UploadResult
-import com.sample.style.AppStylesheet
+import com.sample.style.CommonStyles
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.web.attributes.InputType
 import org.jetbrains.compose.web.css.Style
+import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Input
-import org.jetbrains.compose.web.dom.Text
+import org.jetbrains.compose.web.events.SyntheticChangeEvent
 import org.jetbrains.compose.web.renderComposable
+import org.w3c.dom.HTMLInputElement
 import org.w3c.dom.asList
 import org.w3c.files.File
+
 fun main() {
     renderComposable(rootElementId = "root") {
-        Style(AppStylesheet)
-        val data = remember { mutableStateOf<UploadResult?>(null) }
-        Layout {
-            Input(
-                type = InputType.File,
-                attrs = {
-                    onChange {
-                        val file: File = it.target.files!!.asList().single()
-                        CoroutineScope(Dispatchers.Default).launch {
-                            data.value = HttpEngine.fileUploader.upload(file)
-                        }
-                    }
+        val uploadResult = remember { mutableStateOf<UploadResult?>(null) }
+        Body(
+            onFileInputChange = {
+                val file: File = it.target.files!!.asList().single()
+                CoroutineScope(Dispatchers.Default).launch {
+                    uploadResult.value = HttpEngine.fileUploader.upload(file)
                 }
-            )
-            Text("text=${data.value}")
-        }
+            }
+        )
     }
 }
 
+@Composable
+fun Body(onFileInputChange: (SyntheticChangeEvent<String, HTMLInputElement>) -> Unit) {
+    Style(CommonStyles)
+    Div(
+        attrs = {
+            classes(CommonStyles.horizontalFlow)
+        },
+        content = {
+            Div(
+                {
+                    classes(CommonStyles.p33)
+                },
+                content = {
+                    Div(
+                        attrs = {
+                            classes(CommonStyles.matCard)
+                        },
+                        content = {
+                            FileUploadInput(onChange = onFileInputChange)
+                        }
+                    )
+                }
+            )
+        }
+    )
+}
+
+@Composable
+private fun FileUploadInput(onChange: (SyntheticChangeEvent<String, HTMLInputElement>) -> Unit) {
+    Input(
+        type = InputType.File,
+        attrs = {
+            onChange { it: SyntheticChangeEvent<String, HTMLInputElement> ->
+                onChange(it)
+            }
+        })
+}
--- atlasv-android-home/src/jsMain/kotlin/com/sample/constant/Constants.kt
+++ atlasv-android-home/src/jsMain/kotlin/com/sample/constant/Constants.kt
@@ -4,5 +4,5 @@ package com.sample.constant
  * Created by weiping on 2025/2/10
  */
 object Constants {
-    const val DEBUG = false
+    const val DEBUG = true
 }
