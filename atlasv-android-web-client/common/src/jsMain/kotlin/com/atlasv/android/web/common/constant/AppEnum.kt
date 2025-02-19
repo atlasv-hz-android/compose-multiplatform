da715a3c506ccc290605d92820621dd175cf0913
weiping <weiping@atlasv.com>
2025-02-19 18:48:08 +0800

    feat: 支持更多App

--- atlasv-android-web-client/common/src/jsMain/kotlin/com/atlasv/android/web/common/constant/AppEnum.kt
+++ atlasv-android-web-client/common/src/jsMain/kotlin/com/atlasv/android/web/common/constant/AppEnum.kt
@@ -4,7 +4,12 @@ package com.atlasv.android.web.common.constant
  * Created by weiping on 2025/2/19
  */
 enum class AppEnum(val packageName: String) {
-    ShotCut(packageName = "video.editor.videomaker.effects.fx"),
+    Ins3(packageName = "instagram.video.downloader.story.saver.ig"),
+    MediaSaver(packageName = "instagram.video.downloader.story.saver.ig.insaver"),
+    Ttd1(packageName = "tiktok.video.downloader.nowatermark.tiktokdownload"),
+    Ttd2(packageName = "tiktok.video.downloader.nowatermark.tiktokdownload.snaptik"),
     Fbd2(packageName = "facebook.video.downloader.savefrom.fb.saver.fast"),
-    Ttd2(packageName = "tiktok.video.downloader.nowatermark.tiktokdownload.snaptik")
+//    Twd(packageName = "twittervideodownloader.twitter.videoindir.savegif.twdown"),
+    Ptd(packageName = "pin.pinterest.video.downloader.forpinterest.pinsaver"),
+    ShotCut(packageName = "video.editor.videomaker.effects.fx"),
 }
