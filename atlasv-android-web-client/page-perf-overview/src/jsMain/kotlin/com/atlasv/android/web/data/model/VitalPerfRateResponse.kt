1efcca471126bec5a4dce5e318cdd2e1025a78e2
weiping <weiping@atlasv.com>
2025-02-19 12:45:03 +0800

    fix:  过滤<=3G的机型

--- atlasv-android-web-client/page-perf-overview/src/jsMain/kotlin/com/atlasv/android/web/data/model/VitalPerfRateResponse.kt
+++ atlasv-android-web-client/page-perf-overview/src/jsMain/kotlin/com/atlasv/android/web/data/model/VitalPerfRateResponse.kt
@@ -10,7 +10,9 @@ data class VitalPerfRateResponse(val rows: List<VitalPerfRateModel>) {
     fun flatterByDimensions(): List<VitalPerfRateResponse> {
         val dimensions = this.rows.asSequence().map {
             it.dimensions.orEmpty()
-        }.flatten().filter { it.int64Value < 5000 }.distinctBy { it.int64Value }.sortedByDescending { it.int64Value }
+        }.flatten().filter {
+            it.int64Value < 3072 // 低端机，Ram<=3G。3072算进了3G-4G的范围，故这里用<
+        }.distinctBy { it.int64Value }.sortedByDescending { it.int64Value }
             .toList()
         return dimensions.map { dimension ->
             this.copy(
