b767c70c153925319d2222b21ad08ef09d426108
weiping <weiping@atlasv.com>
2025-04-10 11:29:04 +0800

    feat: 增加文件上传场景

--- atlasv-android-web-client/page-upload-file/src/jsMain/kotlin/com/atlasv/android/web/data/model/BucketType.kt
+++ atlasv-android-web-client/page-upload-file/src/jsMain/kotlin/com/atlasv/android/web/data/model/BucketType.kt
@@ -8,5 +8,7 @@ enum class BucketType(val displayName: String, val bucketName: String, val regio
 
     // https://downloader-media.nyc3.cdn.digitaloceanspaces.com/public/js_parse/prod/fbd/fdown_net.js
     JsCode("Js代码", "downloader-media", "nyc3", "public/js_parse"),
+    // https://downloader-media.nyc3.cdn.digitaloceanspaces.com/public/questionnaires/KOL_ins3_2025_2_jp.json
+    Questionnaire("调查问卷", "downloader-media", "nyc3", "public/questionnaires"),
     Others("其他", "richman-media", "sfo3", "public/events"),
 }
